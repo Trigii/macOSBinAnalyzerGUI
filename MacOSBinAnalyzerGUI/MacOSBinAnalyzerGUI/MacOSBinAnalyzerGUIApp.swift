60e6c879d8215c5a4cd317a97fb7fc4fb6339517
Tristán <tristanvaquero@hotmail.es>
2024-03-22 04:41:23 -0700

    Changed minimum window size

--- MacOSBinAnalyzerGUI/MacOSBinAnalyzerGUI/MacOSBinAnalyzerGUIApp.swift
+++ MacOSBinAnalyzerGUI/MacOSBinAnalyzerGUI/MacOSBinAnalyzerGUIApp.swift
@@ -15,14 +15,12 @@ struct MacOSBinAnalyzerGUI: App {
     var body: some Scene {
         WindowGroup {
             ContentView(databasePath: $databasePath, selectedQuery: $selectedQuery)
-                .frame(minWidth: 1000, minHeight: 800) // Set minimum size for the content view
                 .frame(maxWidth: .infinity, maxHeight: .infinity) // Allow content view to expand
         }
         .windowStyle(HiddenTitleBarWindowStyle()) // Hide title bar for custom frame
         
         WindowGroup("Query Results", id: "run-query") {
             RunQueryView(query: $selectedQuery, databasePath: $databasePath)
-                .frame(minWidth: 1000, minHeight: 800) // Set minimum size for run query view
                 .frame(maxWidth: .infinity, maxHeight: .infinity) // Allow run query view to expand
         }
         .windowStyle(HiddenTitleBarWindowStyle()) // Hide title bar for custom frame
